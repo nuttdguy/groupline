9b74ce89da9960e79786321de15ef34ee553e692
Phouthalang Pygnasak <pygnasak@gmail.com>
2017-10-31 23:19:34 -0700

    added activity routes for guest users

--- routes/activity.js
+++ routes/activity.js
@@ -2,12 +2,28 @@ const express = require('express');
 const router = express.Router();
 
 
-// TODO ::
+//      ACTIVITY ROUTES FOR GUEST USERS     //
 
 //==================================================//
 /*                 /ACTIVITY/                   */
 //==================================================//
 
 router.get('/', function(req, res, next) {
+    // TODO :: get all activity posts
 
+    res.render('/activity/index', {})
 });
+
+
+//==================================================//
+/*              /ACTIVITY/:categoryId             */
+//==================================================//
+
+router.get('/:catId', function(req, res, next) {
+    // TODO :: get activities by category id
+
+    // TODO :: determine view to render
+    res.render('/activity', {})
+});
+
+
