4583e1b42abbd2fb4c5cc4641eb1d6e15085cef0
Phouthalang Pygnasak <pygnasak@gmail.com>
2017-11-14 10:36:26 -0800

    save checkpoint

---
+++ config/passport.js
@@ -0,0 +1,37 @@
+const config = require('./config');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../db/models/userprofile');
+const bcrypt = require('bcrypt');
+
+module.exports = (passport) => {
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+
+        User.findOne({ where: { userProfileId: id } })
+            .then(function(err, user) {
+                done(err, user);
+            });
+    });
+
+    passport.use(new LocalStrategy(function(username, password, done) {
+
+        new User({username: username}).fetch().then(function(data) {
+            var user = data;
+            if (user === null) {
+                return done(null, false, {message: 'Invalid username or password'});
+            } else {
+                user = data.toJSON();
+                if (!bcrypt.compareSync(password, user.password)) {
+                    return done(null, false, {message: 'Invalid password'});
+                } else {
+                    return done(null, user);
+                }
+            }
+        })
+    }))
+
+};
