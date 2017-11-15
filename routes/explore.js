16ced39c17a8a288822916576ae75b85dad38ae2
Phouthalang Pygnasak <pygnasak@gmail.com>
2017-11-15 01:16:44 -0800

    clean out comments

--- routes/explore.js
+++ routes/explore.js
@@ -7,30 +7,20 @@ const Activity = require('../db/models/index').Activity;
 
 
 /* GET home page. */
-router.get('/:id', function(req, res, next) {
-    console.log(req.params.id);
-    // ActivityCategory.findById(req.params.id, { include: Activity}).then((activity) => {
-    //     console.log(activity.dataValues)
-    // })
+router.get('/:id', function (req, res, next) {
 
   Activity.findAll({
       include: [{
         model: ActivityCategory,
         as: 'ActivityCategories',
-        where: { activity_id: req.params.id}} ]}
-        )
-        .then((activity) => {
-          console.log(JSON.stringify(activity));
-          res.render('explore', { title: 'Explore', activity: activity});
+        where: {activity_id: req.params.id}
+      }]
+    }
+  )
+    .then((activity) => {
+      console.log(JSON.stringify(activity));
+      res.render('explore', {title: 'Explore', activity: activity});
     });
-
-  // Activity.findAll({
-  //     include: [ActivityCategory]}
-  //       )
-  //       .then((activity) => {
-  //         console.log(JSON.stringify(activity));
-  //         res.render('explore', { title: 'Explore', });
-  //   });
 
 });
 
