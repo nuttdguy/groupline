9e541bd266ae87ac960c26c8af77aadd1a03105a
Phouthalang Pygnasak <pygnasak@gmail.com>
2017-11-16 12:03:59 -0800

    correct css problem

--- routes/explore_route.js
+++ routes/explore_route.js
@@ -8,42 +8,46 @@ const ActivityTag = require('../db/models/index').ActivityTag;
 const ActivityDetail = require('../db/models/index').ActivityDetail;
 
 
-
 router.get('/', function (req, res, next) {
-  res.redirect('/explore/1')
-
+  res.render('explore', { title: 'Explore' } )
 });
 
 /* GET home page. */
 router.get('/:id', function (req, res, next) {
+  const categories_id = req.params.id;
 
-
-  Activity.findById( req.params.id, {
-      include: [{
-        model: ActivityCategory,
-        as: 'ActivityCategories',
-        where: {activity_id: req.params.id}
-      },
-        {
-          model: ActivityTag,
-          as: 'ActivityTags',
-          where: {activity_id: req.params.id}
+  console.log(categories_id + "=====================");
+  if (categories_id !== undefined) {
+    console.log("THis is the route!!!!!!!!!");
+    Activity.findById(categories_id, {
+        include: [{
+          model: ActivityCategory,
+          as: 'ActivityCategories',
+          where: {activity_id: categories_id}
         },
-        {
-          model: ActivityDetail,
-          as: 'ActivityDetails',
-          where: {activity_id: req.params.id}
-        }]
-    }
-  )
-    .then((activities) => {
-      console.log('========================');
-      console.log(JSON.stringify(activities));
-      res.render('explore', {
-        title: 'Explore',
-        activities: activities
+          {
+            model: ActivityTag,
+            as: 'ActivityTags',
+            where: {activity_id: categories_id}
+          },
+          {
+            model: ActivityDetail,
+            as: 'ActivityDetails',
+            where: {activity_id: categories_id}
+          }]
+      }
+    )
+      .then((activities) => {
+        console.log('========================');
+        console.log(JSON.stringify(activities));
+        res.render('explore', {
+          title: 'Explore',
+          activities: activities
+        });
       });
-    });
+  }
+
+  // res.redirect('/')
 
 });
 
